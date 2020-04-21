aae880f0b88699e400cda884484080a6b9f9e4f5
GodJunie <juniesoft@gmail.com>
2020-04-21 18:48:27 +0900

    Data Sheet 불러오기 예시

---
+++ Assets/0.GodJunie/Scripts/DataManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GodJunie.QGJ2020 {
+    public class DataManager : MonoBehaviour {
+        #region Instance
+        protected static DataManager sInstance = null;
+
+        public static DataManager Instance {
+            get {
+                if(sInstance == null) {
+                    sInstance = FindObjectOfType(typeof(DataManager)) as DataManager;
+
+                    if(sInstance == null) {
+                        Debug.Log("Nothing" + sInstance.ToString());
+                        return null;
+                    }
+                }
+                return sInstance;
+            }
+        }
+        #endregion
+
+        [SerializeField] private DataSheet datasheetTable;
+        private List<DataSheetData> dataList;
+
+        private void Awake() {
+            dataList = datasheetTable.dataArray.ToList();
+        }
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public DataSheetData GetData(string id) {
+            return dataList.Find(i => i.Ed == id);
+        }
+
+        public DataSheetData GetData(int index) {
+            return dataList[index];
+        }
+    }
+}
--- Assets/Scripts/RunTime/DataSheetData.cs
+++ Assets/Scripts/RunTime/DataSheetData.cs
@@ -8,4 +8,56 @@ using System.Collections;
 [System.Serializable]
 public class DataSheetData
 {
+  [SerializeField]
+  string mainact;
+  public string Mainact { get {return mainact; } set { this.mainact = value;} }
+  
+  [SerializeField]
+  string mainact_desc;
+  public string Mainact_Desc { get {return mainact_desc; } set { this.mainact_desc = value;} }
+  
+  [SerializeField]
+  string subact_id;
+  public string Subact_ID { get {return subact_id; } set { this.subact_id = value;} }
+  
+  [SerializeField]
+  string subact;
+  public string Subact { get {return subact; } set { this.subact = value;} }
+  
+  [SerializeField]
+  string subact_desc;
+  public string Subact_Desc { get {return subact_desc; } set { this.subact_desc = value;} }
+  
+  [SerializeField]
+  string finalact_id;
+  public string Finalact_ID { get {return finalact_id; } set { this.finalact_id = value;} }
+  
+  [SerializeField]
+  string finalact;
+  public string Finalact { get {return finalact; } set { this.finalact = value;} }
+  
+  [SerializeField]
+  string finalact_desc;
+  public string Finalact_Desc { get {return finalact_desc; } set { this.finalact_desc = value;} }
+  
+  [SerializeField]
+  string hungry;
+  public string Hungry { get {return hungry; } set { this.hungry = value;} }
+  
+  [SerializeField]
+  string clean;
+  public string Clean { get {return clean; } set { this.clean = value;} }
+  
+  [SerializeField]
+  string mental;
+  public string Mental { get {return mental; } set { this.mental = value;} }
+  
+  [SerializeField]
+  string cost;
+  public string Cost { get {return cost; } set { this.cost = value;} }
+  
+  [SerializeField]
+  string ed;
+  public string Ed { get {return ed; } set { this.ed = value;} }
+  
 }
